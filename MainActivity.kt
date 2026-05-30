f58cbb012f7c167933de647d220151cc4e51fd0a
VIVEK <vivekgupta082003@gmail.com>
2026-05-30 10:12:09 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.recall_dialogue
+package com.example.recall_dialogue;
 
 import android.os.Bundle
 import android.widget.Button
@@ -6,66 +6,69 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+
+
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocket
+import okhttp3.WebSocketListener
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var webSocketListener: Eachitem
-    private lateinit var mainViewModel: MainViewModel
-    private val okHttpClient = OkHttpClient()
-    private var webSocket: WebSocket? = null
-
+class MainActivity : AppCompatActivity(){
+    lateinit var webStocketListener: Eachitem
+    lateinit var mainViewHolder: Custom_dailogue
+    private val okHttpClient= OkHttpClient()
+    private var webSocket: WebSocket?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnConnect = findViewById<Button>(R.id.connect)
-        val btnDisconnect = findViewById<Button>(R.id.disconnect)
-        val tvMessage = findViewById<TextView>(R.id.message)
-        val editMessage = findViewById<EditText>(R.id.editMessage)
-        val btnSend = findViewById<Button>(R.id.send)
+        val btnConnect=findViewById<Button>(R.id.connect)
+        val btnDisconnnect=findViewById<Button>(R.id.disconnect)
+        val tvMessage=findViewById<TextView>(R.id.message)
+        val editMessage=findViewById<EditText>(R.id.editMessage)
+        val btnSend=findViewById<Button>(R.id.send)
 
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        webSocketListener = Eachitem(mainViewModel)
+        mainViewHolder= ViewModelProvider(this)[Custom_dailogue::class.java]
+        webStocketListener= Eachitem(mainViewHolder)
 
-        mainViewModel.socketStatus.observe(this, Observer { isConnected ->
-            tvMessage.text = if (isConnected) "Connected" else "Disconnected"
+        mainViewHolder.socketStatus.observe(this, Observer{isConnect ->
+            tvMessage.text=if (isConnect)"Connect" else "Disconnected"
         })
-
-        var chatHistory = ""
-        mainViewModel.message.observe(this, Observer { pair ->
-            chatHistory += "${if (pair.first) "Other: " else "You: "}${pair.second}\n"
-            tvMessage.text = chatHistory
-        })
-
+        var chatHistory=""
+      mainViewHolder.message.observe(this, Observer{pair ->
+          chatHistory +="${if(pair.first)"other:" else "You: "}${pair.second}\n"
+          tvMessage.text=chatHistory
+      })
         btnConnect.setOnClickListener {
-            if (webSocket == null) {
-                webSocket = okHttpClient.newWebSocket(createRequest(), webSocketListener)
+            if (webSocket==null){
+            webSocket=okHttpClient.newWebSocket(createRequest(), webStocketListener)
             }
         }
-
-        btnDisconnect.setOnClickListener {
-            webSocket?.close(1000, "Cancelled Manually")
-            webSocket = null
+        btnDisconnnect.setOnClickListener {
+            webSocket?.close(1000,"Cancelled Manually")
+            webSocket=null
         }
-
         btnSend.setOnClickListener {
-            val message = editMessage.text.toString()
-            if (message.isNotEmpty() && webSocket != null) {
+            val message=editMessage.text.toString()
+            if (message.isNotEmpty()&& webSocket !=null){
                 webSocket?.send(message)
-                chatHistory += "You: $message\n"
-                tvMessage.text = chatHistory
+                chatHistory+="You:$message\n"
+                tvMessage.text=chatHistory
                 editMessage.text.clear()
             }
         }
+
+
+
     }
 
-    private fun createRequest(): Request {
+    fun createRequest(): Request {
         val webSocketUrl = "wss://s18475.nyc1.piesocket.com/v3/1?api_key=h7inTrnho3OthWRSNMoKOYT36P56kNBLyrCY8MIt&notify_self=1"
         return Request.Builder()
             .url(webSocketUrl)
             .build()
     }
+
 }
